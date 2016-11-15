90b36d634bc71a564b894312819d6be94d080d72
britbroet <brittanybroetje@gmail.com>
2016-11-15 09:16:03 -0800

    guessing game

--- lib/guess.rb
+++ lib/guess.rb
@@ -3,16 +3,24 @@ def guessing(n)
 	max = 100
 	tries = 0
 
-	while min <= max
-		puts "Guess a number between 1 and 100"
-		guess = gets.chomp.to_i
+	puts "Guess a number between 1 and 100"
 
-	
+	while min <= max
+		guess = gets.chomp.to_i
+		tries = tries + 1
+
 		if guess == n
-			puts "You got it in " + tries
+			puts "You got it in " + tries.to_s
+		elsif guess > n
+			puts "The number is lower than " + guess.to_s + ". Guess again."
+		elsif guess < n
+			puts "The number is higher than " + guess.to_s + ". Guess again."
+		end
+	end
+
 end
 
-guessing(5)
+guessing(15)
 
 
 
